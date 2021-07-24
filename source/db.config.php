44afc78a50167d108a31824b1007398cfaa0dec0
Faical El Jabli <57249962+scrucher@users.noreply.github.com>
2021-07-24 17:00:27 +0100

    Update db.config.php

--- source/db.config.php
+++ source/db.config.php
@@ -6,7 +6,7 @@ session_start();
 $servername = "ec2-54-155-208-5.eu-west-1.compute.amazonaws.com";
 $username = "nleqlzqkrtkews";
 $password = "71d4758c4df7d85cdb0b675c954127665732c156636394bf834d8e9e76b81c0e";
-$port     = 5432
+$port     = 5432;
 
 try 
 {
