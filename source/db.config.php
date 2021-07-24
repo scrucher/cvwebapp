bec8c724956f3484ae9188370b778a370b8a346a
Faical El Jabli <57249962+scrucher@users.noreply.github.com>
2021-07-24 16:56:26 +0100

    Update db.config.php

--- source/db.config.php
+++ source/db.config.php
@@ -3,13 +3,14 @@
 ob_start();
 session_start();
 
-$servername = "localhost";
-$username = "root";
-$password = "password";
+$servername = "ec2-54-155-208-5.eu-west-1.compute.amazonaws.com";
+$username = "nleqlzqkrtkews";
+$password = "71d4758c4df7d85cdb0b675c954127665732c156636394bf834d8e9e76b81c0e";
+$port     = 5432
 
 try 
 {
-  $conn = new PDO("mysql:host=$servername;dbname=cvapp", $username, $password);
+  $conn = new PDO("pgsql:host=$servername;dbname=d4aiumim8ubo56", $username, $password,$port);
 
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
